90ade51e178cdde5440600e0875e636b97996606
Igor-Polonskiy <53859088+Igor-Polonskiy@users.noreply.github.com>
2021-03-09 17:31:07 +0300

    feat: min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array!=undefined) {
+        if (array.length == 0) {
+            return 0
+        } else 
+        array.sort((a, b) => a - b);
+            return array[0];
+    }
+    return 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array) {
+        if (array[0] == undefined) {
+            return 0
+        }
+        array.sort((a, b) => b - a);
+        return array[0];
+    }
+    return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array) {
+        if (array[0] == undefined) {
+            return 0
+        }
+        let summ = 0
+        array.forEach(item => {
+            summ += item
+        })
+        return summ / array.length
+    } else
+        return 0
 }
